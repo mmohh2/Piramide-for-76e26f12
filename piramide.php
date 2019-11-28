3f75cd0f34f1328293c68985882b5d1ab875c35b
Mo <moh19951@outlook.com>
2019-11-28 10:33:32 +0100

    Uitwerking van de exercise

--- piramide.php
+++ piramide.php
@@ -4,7 +4,7 @@ echo "Hoeveel stapels wil je hebben?";
 $a = readline(">");
 for ($i = 1; $i < 5; $i++) {
     for ($j = $i; $j <= 5; $j++) {
-        echo "\n"; // it will print blank space
+        echo "\n"; 
     }
     for ($j = 1; $j <= $i; $j++) {
         echo " * ";
